768a42361bfd52a3e8a8e320fdae01b1b9445b29
KeigoHarada <hkddna5@gmail.com>
2024-08-30 00:29:30 +0900

    home画面にアイコン追加

--- src/components/MonthlySummary.tsx
+++ src/components/MonthlySummary.tsx
@@ -1,13 +1,16 @@
 import { Card, CardContent, Grid2, Stack, Typography } from '@mui/material'
 import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import React from 'react'
 
 const MonthlySummary = () => {
     return (
-        <Grid2 container >
-            <Grid2>
-                <Card sx={{ bgcolor: "blue", color: "white", borderRadius: "10px" }}>
-                    <CardContent>
+        <Grid2 container spacing={{ xs: 1, sm: 2 }}>
+            {/* 収入 */}
+            < Grid2 size={4} display={"flex"} flexDirection={"column"}>
+                <Card sx={{ bgcolor: "blue", color: "white", borderRadius: "10px", flexGrow: 1 }}>
+                    <CardContent sx={{ padding: { xs: 1, sm: 2 } }}>
                         <Stack direction={"row"}>
                             <KeyboardDoubleArrowUpIcon sx={{ fontSize: "2rem" }} />
                             <Typography variant="h5">収入</Typography>
@@ -16,41 +19,54 @@ const MonthlySummary = () => {
                             textAlign={"right"}
                             variant="h5"
                             fontWeight={"bold"}
-                            sx={{ wordBreak: "break-word" }}>¥100,000</Typography>
+                            sx={{
+                                wordBreak: "break-word",
+                                fontSize: { xs: "0.8rem", sm: "1rem", md: "1.2rem" }
+                            }}>¥100,000</Typography>
                     </CardContent>
                 </Card>
-            </Grid2>
-            <Grid2>
-                <Card sx={{ bgcolor: "blue", color: "white", borderRadius: "10px" }}>
-                    <CardContent>
+            </Grid2 >
+
+            {/* 支出 */}
+            < Grid2 size={4} display={"flex"} flexDirection={"column"}>
+                <Card sx={{ bgcolor: "red", color: "white", borderRadius: "10px", flexGrow: 1 }}>
+                    <CardContent sx={{ padding: { xs: 1, sm: 2 } }}>
                         <Stack direction={"row"}>
-                            <KeyboardDoubleArrowUpIcon sx={{ fontSize: "2rem" }} />
-                            <Typography variant="h5">収入</Typography>
+                            <KeyboardDoubleArrowDownIcon sx={{ fontSize: "2rem" }} />
+                            <Typography variant="h5">支出</Typography>
                         </Stack>
                         <Typography
                             textAlign={"right"}
                             variant="h5"
                             fontWeight={"bold"}
-                            sx={{ wordBreak: "break-word" }}>¥100,000</Typography>
+                            sx={{
+                                wordBreak: "break-word",
+                                fontSize: { xs: "0.8rem", sm: "1rem", md: "1.2rem" }
+                            }}>¥100,000</Typography>
                     </CardContent>
                 </Card>
-            </Grid2>
-            <Grid2>
-                <Card sx={{ bgcolor: "blue", color: "white", borderRadius: "10px" }}>
-                    <CardContent>
+            </Grid2 >
+
+            {/* 残高 */}
+            < Grid2 size={4} display={"flex"} flexDirection={"column"}>
+                <Card sx={{ bgcolor: "green", color: "white", borderRadius: "10px", flexGrow: 1 }}>
+                    <CardContent sx={{ padding: { xs: 1, sm: 2 } }}>
                         <Stack direction={"row"}>
-                            <KeyboardDoubleArrowUpIcon sx={{ fontSize: "2rem" }} />
-                            <Typography variant="h5">収入</Typography>
+                            <AccountBalanceIcon sx={{ fontSize: "2rem" }} />
+                            <Typography variant="h5">残高</Typography>
                         </Stack>
                         <Typography
                             textAlign={"right"}
                             variant="h5"
                             fontWeight={"bold"}
-                            sx={{ wordBreak: "break-word" }}>¥100,000</Typography>
+                            sx={{
+                                wordBreak: "break-word",
+                                fontSize: { xs: "0.8rem", sm: "1rem", md: "1.2rem" }
+                            }}>¥100,000</Typography>
                     </CardContent>
                 </Card>
-            </Grid2>
-        </Grid2>
+            </Grid2 >
+        </Grid2 >
 
 
     )
